a596eb0d0fdccaa1db243ed924f3d7ba04af9979
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2017-11-28 14:15:55 +0300

    Configure postgres DB

--- config/db.go
+++ config/db.go
@@ -1,1 +1,35 @@
 package config
+
+import (
+	"os"
+
+	"github.com/SausageRocketDatingService/SausageRocketApi/models"
+	"github.com/go-xorm/xorm"
+	_ "github.com/lib/pq" // driver for postgresql
+)
+
+// InitDB initializes postgres DB and returns engine of type xorm.Engine and error
+func InitDB() (*xorm.Engine, error) {
+	engine, err := xorm.NewEngine("postgres", os.Getenv("DATABASE_URL"))
+	return engine, err
+}
+
+// CreateDBTables generates DB Tables for DB Models
+func CreateDBTables(engine *xorm.Engine) error {
+	userError := models.CreateUserTable(engine)
+	if userError != nil {
+		return userError
+	}
+
+	preferencesError := models.CreateUserPreferencesTable(engine)
+	if preferencesError != nil {
+		return preferencesError
+	}
+
+	ratingError := models.CreateUserRatingTable(engine)
+	if ratingError != nil {
+		return ratingError
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,18 +1,45 @@
 package main
 
 import (
+	"log"
+	"os"
+
+	"github.com/SausageRocketDatingService/SausageRocketApi/config"
 	"github.com/SausageRocketDatingService/SausageRocketApi/handlers"
+	"github.com/joho/godotenv"
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/middleware/logger"
 	"github.com/kataras/iris/middleware/recover"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
 	app := iris.New()
 	app.Use(recover.New())
 	app.Use(logger.New())
 
+	orm, dbError := config.InitDB()
+	if dbError != nil {
+		app.Logger().Fatalf("Failed to initialize DB: %v", dbError)
+	}
+
+	dbError = config.CreateDBTables(orm)
+
+	if dbError != nil {
+		app.Logger().Fatalf("Failed to initialize DB Tables: %v", dbError)
+	}
+
+	iris.RegisterOnInterrupt(func() {
+		orm.Close()
+	})
+
 	app.Get("/", handlers.HelloWorld)
 
-	app.Run(iris.Addr(":5000"))
+	app.Run(iris.Addr(":" + port))
 }
